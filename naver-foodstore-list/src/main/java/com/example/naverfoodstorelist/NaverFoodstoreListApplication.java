8ecf140df227a5de50a7853906f6fb464fd13e61
S-Onix <msyoo101@hansung.ac.kr>
2022-09-18 19:50:10 +0900

    project create

---
+++ naver-foodstore-list/src/main/java/com/example/naverfoodstorelist/NaverFoodstoreListApplication.java
@@ -0,0 +1,13 @@
+package com.example.naverfoodstorelist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaverFoodstoreListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaverFoodstoreListApplication.class, args);
+	}
+
+}
---
+++ naver-foodstore-list/src/test/java/com/example/naverfoodstorelist/NaverFoodstoreListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.naverfoodstorelist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaverFoodstoreListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
